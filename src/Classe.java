2082c2f7b8a9f40be08b21b339e3999eaf360eb0
youssfmadani <youssfelmadani@gmail.com>
2024-12-17 10:42:31 +0100

    Ajout de la classe Classe avec gestion des apprenants et formateur, et méthode afficherDetails.

---
+++ src/Classe.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class Classe {
+    private String nom;
+    private Formateur formateur;
+    private ArrayList<Apprenant> apprenants = new ArrayList<>();
+
+    public Classe(String nom, Formateur formateur) {
+        this.nom = nom;
+        this.formateur = formateur;
+    }
+
+    public void ajouterApprenant(Apprenant apprenant) {
+        apprenants.add(apprenant);
+    }
+
+    public void afficherDetails() {
+        System.out.println("Class: " + nom + " - Formateur: " + (formateur != null ? formateur.getNom() : "Non assigné"));
+        System.out.println("Apprenants: ");
+        for (Apprenant a : apprenants) {
+            System.out.println("- " + a.getNom() + " " + a.getPrenom());
+        }
+    }
+
+    public Formateur getFormateur() {
+        return formateur;
+    }
+    public void setFormateur(Formateur formateur) {
+        this.formateur = formateur;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public ArrayList<Apprenant> getApprenants() {
+        return apprenants;
+    }
+    public void setApprenants(ArrayList<Apprenant> apprenants) {
+        this.apprenants = apprenants;
+    }
+}
